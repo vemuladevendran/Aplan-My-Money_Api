a45f43d436080ade1f0ab120e8dce6e07714d886
vemuladevendran <devendranvemula@gmail.com>
2024-08-19 16:59:35 -0700

    clean

--- index.js
+++ index.js
@@ -2,34 +2,36 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-require('./db')
+require('./db');
 const userRoutes = require("./routes/user");
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 app.use(express.json());
+
+// Allow all origins with CORS
 app.use(cors());
+
 app.use("/static", express.static("uploads"));
 
 // routes
 userRoutes(app);
-
 
 // error middleware 
 app.use((error, req, res, next) => {
     res.status(500).json({
         message: error.message,
         error: 'Something went wrong please try later'
-    })
-})
+    });
+});
 
 // server listening on the port 3000
-const PORT = 3000 
-app.listen(PORT, ()=> {
-    console.log(`Server is listering on the port ${PORT}`);
-})
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on the port ${PORT}`);
+});
